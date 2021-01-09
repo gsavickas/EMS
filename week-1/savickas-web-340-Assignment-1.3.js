8f9e0837bbfa3e95214e733724b77258715d50c0
Grayton Savickas <gsavickaspro@gmail.com>
2021-01-08 20:18:35 -0600

    Adds assignment 1.4

--- week-1/savickas-web-340-Assignment-1.3.js
+++ week-1/savickas-web-340-Assignment-1.3.js
@@ -4,13 +4,13 @@
 ; Author: Grayton Savickas
 ; Date:   7 Jan 2021
 ; Modified By: 
-; Description: Simulating classical programming in JavaScript 
+; Description: program that uses node to parse a url
 ;===========================================
 */
 
 const header = require('../week-1/Savickas-header')
 // Tests the import of header function works
-var headerValue = header.display("Grayton", "Savickas", "Exercise 1.3 - Class Refresher")
+var headerValue = header.display("Grayton", "Savickas", "Exercise 1.3 - Modules")
 console.log(headerValue);
 
 // Start of Program
@@ -18,6 +18,7 @@ console.log(headerValue);
 // I looked up the Node 15 documentation and this is the new URL object that we can use instead of the parse method 
 let url1 = new URL('https:www.example.com/profile?name=savickas');
 
+// Output
 console.log(url1.protocol);
 console.log(url1.host);
 // I had to change this from to .search method
