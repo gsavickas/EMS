ef292a9db3d3b9c73972babbe84bfc0b246cf482
Grayton Savickas <graytonsavickas@Mandalorian.local>
2021-01-17 19:15:44 -0600

    adds assignment 2.4

--- week-2/ejs-views/savickas-Assignment-2.4.js
+++ week-2/ejs-views/savickas-Assignment-2.4.js
@@ -2,8 +2,8 @@
 ============================================
 ; Title:  Exercise 2.4 - EJS
 ; Author: Grayton Savickas
-; Date:   16 Jan 2021
-; Modified By: 
+; Date:   17 Jan 2021
+; Modified By:
 ; Description: Tests out using EJS
 ;===========================================
 */
@@ -17,4 +17,24 @@ var http = require("http");
 var path = require("path");
 var app = express();
 
+// Shows the views
+app.set('views', path.resolve(__dirname, '../views'))
 app.set("view engine", "ejs");
+
+app.get('/', function(req, res) {
+  res.render('views', {
+    message: 'Welcome to the homepage!'
+  });
+});
+
+// app.get("/", function(require, response){
+//     response.render("index.ejs",{
+//       firstName:"Grayton",
+//       lastName:"Savickas"
+//     });
+// });
+
+http.createServer(app).listen(8080, function(){
+  console.log("EJS-Views app started on port 8080.");
+});
+
