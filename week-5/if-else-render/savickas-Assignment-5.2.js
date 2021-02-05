71fc88527af847f6c25e37dc09b81c3a93fbb3a1
Grayton Savickas <graytonsavickas@Mandalorian.local>
2021-02-04 20:41:13 -0600

    adds to assignment 5.2

---
+++ week-5/if-else-render/savickas-Assignment-5.2.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title:  Exercise 5.2 - if else render
+; Author: Grayton Savickas
+; Date:   31 Jan 2021
+; Modified By:
+; Description: Crud Operations
+;===========================================
+*/
+const header = require('../../week-1/Savickas-header')
+// Tests the import of header function works
+var headerValue = header.display("Grayton", "Savickas", "Exercise 5.2 - if else render")
+console.log(headerValue);
+
+// start of program
+
