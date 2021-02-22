1b112f91c4bd0213987ce16ba755b648705f248b
Grayton Savickas <gsavickaspro@gmail.com>
2021-02-21 22:53:12 -0600

    adds skeleton updates for 7.4

---
+++ ems/models/employee.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title:  Exercise 7.4
+; Author: Grayton Savickas
+; Date:   21 Feb 2021
+; Modified By:
+; Description: Development of UI elements in EJS
+;===========================================
+*/
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let EmployeeSchema = new Schema({
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true}
+})
+
+module.exports = mongoose.model('employee', EmployeeSchema);
