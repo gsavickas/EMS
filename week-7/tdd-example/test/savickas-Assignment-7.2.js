bef37325e2656cea00346bc61d4965a9784af89a
Grayton Savickas <gsavickaspro@gmail.com>
2021-03-07 15:12:26 -0600

    Delete week-7 directory

--- week-7/tdd-example/test/savickas-Assignment-7.2.js
+++
@@ -1,7 +0,0 @@
-const assert = require('../savickas-Assignment-7.2');
-
-describe("Assignment 7.2 Str.split", function(){
-  it("Should return an array of fruits that is comma separated", function(){
-    assert(Array.isArray('Apple,Orange,Mango'.split(',')));
-  })
-})
